c34641135a83a1073c5d64ee8c8e5ddf942896e5
Julius Grosse <jgrosse@lcs-mba3212.lcs.on.ca>
2017-11-16 09:34:34 -0500

    got to work it with a loop

--- 50 Blank Canvases.playground/Pages/Rectangle.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Rectangle.xcplaygroundpage/Contents.swift
@@ -13,39 +13,22 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 400, height: 400)
 
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-canvas.defaultLineWidth = 10
+
 // Step 1: Move to starting pointabout
 canvas.translate(byX: 75, byY: 100)
 canvas.drawAxes()
+canvas.defaultLineWidth = 10
 
-// Step 2: Draw a side of the triangle
-canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
-canvas.translate(byX: 200, byY: 0)
-canvas.rotate(by: 90)
+// Make the loop to draw the sides
+for _ in 1...4 {
+    // Step 2: Draw a side of the triangle
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+    canvas.translate(byX: 200, byY: 0)
+    canvas.rotate(by: 90)
 
-// Step 3: Draw a side of the triangle
-canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
-canvas.translate(byX: 200, byY: 0)
-canvas.rotate(by: 90)
-
-// Step 3: Draw a side of the triangle
-canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
-canvas.translate(byX: 200, byY: 0)
-canvas.rotate(by: 90)
-
-// Step 4: Draw a side of the triangle
-canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
-canvas.translate(byX: 200, byY: 0)
-canvas.rotate(by: 90)
+}
 
 
 
